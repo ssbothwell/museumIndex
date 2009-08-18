45ee01e3d4433cb33eda37b23ca95095b4e37b2e
Solomon Bothwell <solomon@yellowshark.gateway.2wire.net>
2009-08-18 16:30:36 -0700

    changed deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,13 +30,4 @@ namespace :deploy do
     run "touch #{current_path}/tmp/restart.txt"
   end
 
-  desc "Symlink shared configs and folders on each release."  
-  task :symlink_shared do
-    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-    run "ln -nfs #{shared_path}/db/production.sqlite3 #{release_path}/db/production.sqlite3"    
-    run "ln -nfs #{shared_path}/assets #{release_path}/public/assets"    
-  end
-  
 end
-
-after 'deploy:update_code', 'deploy:symlink_shared'
