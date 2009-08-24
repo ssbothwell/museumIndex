976fbce108ce9bd911f138c3c75caf39869d99e3
Solomon Bothwell <solomon@yellowshark.gateway.2wire.net>
2009-08-24 02:47:13 -0700

    replaced to_param to create more friendly urls

--- app/models/exhibition.rb
+++ app/models/exhibition.rb
@@ -1,4 +1,8 @@
 class Exhibition < ActiveRecord::Base
   validates_uniqueness_of :title
   belongs_to :museum
+  
+  def to_param
+    "#{id}-#{title.gsub(/[^a-z1-9]+/i, '-')}"
+  end
 end
--- app/models/museum.rb
+++ app/models/museum.rb
@@ -1,3 +1,8 @@
 class Museum < ActiveRecord::Base
   has_many :exhibitions
+  
+  def to_param
+    "#{id}-#{name.gsub(/[^a-z1-9]+/i, '-')}"
+  end
+  
 end
