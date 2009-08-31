357d8c695120843889e1e1b87b28567c58af1297
Solomon Bothwell <solomon@yellowshark.gateway.2wire.net>
2009-08-31 00:22:14 -0700

    removed cronjob from deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,11 +29,3 @@ namespace :deploy do
     run "touch #{current_path}/tmp/restart.txt"
   end
 end
-
-after "deploy:symlink", "deploy:update_crontab"
-namespace :deploy do
-  desc "Update the crontab file"
-  task :update_crontab, :roles => :db do
-    run "cd #{release_path} && whenever --update-crontab #{application}"
-  end
-end
