eb97f0ada54d26e227a54b5ddccc62a5d6d536ad
Solomon Bothwell <solomon@yellowshark.gateway.2wire.net>
2009-09-03 03:35:08 -0700

    A little js for search bar.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,21 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+$(document).ready(function() {
+		$("#search_date_open_gte").focus(function(){ 
+			if (this.value == 'From'){
+			this.value='' }
+		});
+		$("#search_date_open_gte").blur(function(){ 
+			if (this.value == ''){
+			this.value='From' }
+		});
+		$("#search_date_close_lte").focus(function(){ 
+			if (this.value == 'To'){
+			this.value='' }
+		});
+		$("#search_date_close_lte").blur(function(){ 
+			if (this.value == ''){
+			this.value='To' }
+		});
+});
+
